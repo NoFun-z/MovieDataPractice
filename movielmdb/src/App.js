918b893871302053cfc084c0bd1150f6f9a4b028
NoFun <hpham15@ncstudents.niagaracollege.ca>
2023-06-27 19:47:02 -0400

    Half way through

--- movielmdb/src/App.js
+++ movielmdb/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './Components/header/Header'
+import Home from './pages/home/home'
 
 export default function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
-          <Route index element ={<h1>Okay</h1>}></Route>
+          <Route index element={<Home/>}></Route>
           <Route path="movie/:id" element={<h1>Movie Details</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Movie list page</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movies List</h1>}></Route>
           <Route path="/*" element={<h1>Error, Page not found</h1>}></Route>
         </Routes>
       </Router>
